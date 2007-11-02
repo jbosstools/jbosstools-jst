976d14b403fc8d51781423d95e6e20509429df6f
Denis Golovin <dgolovin@exadel.com>
2007-11-02 04:39:06 +0000

    http://jira.jboss.com/jira/browse/JBIDE-1244
    NPE fixed
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@4651 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.jst.jsp/src/org/jboss/tools/jst/jsp/editor/TLDRegisterHelper.java
+++ plugins/org.jboss.tools.jst.jsp/src/org/jboss/tools/jst/jsp/editor/TLDRegisterHelper.java
@@ -13,6 +13,7 @@ package org.jboss.tools.jst.jsp.editor;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.ui.IEditorInput;
+import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IFileEditorInput;
 import org.jboss.tools.common.kb.KbTldResource;
 import org.jboss.tools.common.kb.wtp.JspWtpKbConnector;
@@ -51,12 +52,16 @@ public class TLDRegisterHelper {
 					JspEditorPlugin.getPluginLog().logError(e);
 					return;
 				}
-	        	IEditorInput input = JspEditorPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor().getEditorInput();
-	        	String tldContent = DropUtils.getTldContent(input, data.getUri());
-	        	if(tldContent!=null) {
-	        		resource.setTldContent(tldContent);
-	        		wtpKbConnector.registerResource(resource, true);
-	        	}
+	        	
+	        		IEditorPart editorPart = JspEditorPlugin.getDefault().getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+	        		if(editorPart!=null) {
+	        				IEditorInput input = editorPart.getEditorInput();
+	        				String tldContent = DropUtils.getTldContent(input, data.getUri());
+	        				if(tldContent!=null) {
+	        	        		resource.setTldContent(tldContent);
+	        	        		wtpKbConnector.registerResource(resource, true);
+	        	        	}
+	        		}
 	        }
 		} else {
 			wtpKbConnector.registerResource(resource);
