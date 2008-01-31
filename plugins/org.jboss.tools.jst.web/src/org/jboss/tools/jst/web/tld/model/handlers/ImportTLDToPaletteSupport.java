495cd731470b0369ae8370b0e44ea15088d4ffdd
Viacheslav Kabanovich <scabanovich@exadel.com>
2008-01-31 11:59:12 +0000

    JBIDE-1711
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@6064 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.jst.web/src/org/jboss/tools/jst/web/tld/model/handlers/ImportTLDToPaletteSupport.java
+++ plugins/org.jboss.tools.jst.web/src/org/jboss/tools/jst/web/tld/model/handlers/ImportTLDToPaletteSupport.java
@@ -71,7 +71,7 @@ public class ImportTLDToPaletteSupport extends SpecialWizardSupport {
 	}
 	
 	public void action(String name) throws Exception {
-		if(OK.equals(name)) {
+		if(OK.equals(name) || FINISH.equals(name)) {
 			execute();
 			setFinished(true);
 		} else if(CANCEL.equals(name)) {
