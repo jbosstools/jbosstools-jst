9f83aee4b9378bfb8ce3195629cf39d3ecbaed65
Viacheslav Kabanovich <scabanovich@exadel.com>
2008-01-31 12:53:57 +0000

    JBIDE-1711
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@6073 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.jst.web/src/org/jboss/tools/jst/web/project/handlers/AddJarToLibrarySetSupport.java
+++ plugins/org.jboss.tools.jst.web/src/org/jboss/tools/jst/web/project/handlers/AddJarToLibrarySetSupport.java
@@ -41,7 +41,7 @@ public class AddJarToLibrarySetSupport extends SpecialWizardSupport {
 	}
 
 	public void action(String name) throws Exception {
-		if(OK.equals(name)) {
+		if(OK.equals(name) || FINISH.equals(name)) {
 			execute();
 			setFinished(true);
 		} else if(CANCEL.equals(name)) {
