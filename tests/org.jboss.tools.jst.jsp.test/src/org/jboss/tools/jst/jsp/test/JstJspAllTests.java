c31553b9711ebf609273ee00bfabd8943c7f5c96
Victor Rubezhny <ruby@exadel.com>
2008-02-11 19:37:10 +0000

    http://jira.jboss.org/jira/browse/JBIDE-1766 Create a JUnit TestCase for JBIDE-1759
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@6248 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.jst.jsp.test/src/org/jboss/tools/jst/jsp/test/JstJspAllTests.java
+++ tests/org.jboss.tools.jst.jsp.test/src/org/jboss/tools/jst/jsp/test/JstJspAllTests.java
@@ -4,6 +4,7 @@ import org.jboss.tools.jst.jsp.test.ca.JsfJspJbide1704Test;
 import org.jboss.tools.jst.jsp.test.ca.JsfJspJbide1717Test;
 import org.jboss.tools.jst.jsp.test.ca.JstJspJbide1585Test;
 import org.jboss.tools.jst.jsp.test.ca.JstJspJbide1641Test;
+import org.jboss.tools.jst.jsp.test.ca.JstJspJbide1759Test;
 import org.jboss.tools.jst.jsp.test.ca.StrutsJspJbide1648Test;
 
 import junit.framework.Test;
@@ -13,11 +14,13 @@ public class JstJspAllTests {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for org.jboss.tools.jst.jsp.test");
-		suite.addTestSuite(JstJspJbide1585Test.class);
+		
+ 		suite.addTestSuite(JstJspJbide1585Test.class);
 		suite.addTestSuite(StrutsJspJbide1648Test.class);
 		suite.addTestSuite(JstJspJbide1641Test.class);
 		suite.addTestSuite(JsfJspJbide1704Test.class);
 		suite.addTestSuite(JsfJspJbide1717Test.class);
+		suite.addTestSuite(JstJspJbide1759Test.class);
 		return suite;
 	}
 
