92b9c78b1068d1e49e10f44404c4e916e0df6dc7
Eugene Stherbin <estherbin@exadel.com>
2008-06-10 09:27:51 +0000

    Write junit for http://jira.jboss.com/jira/browse/JBIDE-1791 issue.
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@8663 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.jst.jsp.test/src/org/jboss/tools/jst/jsp/test/JstJspAllTests.java
+++ tests/org.jboss.tools.jst.jsp.test/src/org/jboss/tools/jst/jsp/test/JstJspAllTests.java
@@ -1,5 +1,9 @@
 package org.jboss.tools.jst.jsp.test;
 
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.jboss.tools.jst.jsp.test.ca.Jbide1791Test;
 import org.jboss.tools.jst.jsp.test.ca.JsfJspJbide1704Test;
 import org.jboss.tools.jst.jsp.test.ca.JsfJspJbide1717Test;
 import org.jboss.tools.jst.jsp.test.ca.JsfJspJbide1807Test;
@@ -8,9 +12,6 @@ import org.jboss.tools.jst.jsp.test.ca.JstJspJbide1585Test;
 import org.jboss.tools.jst.jsp.test.ca.JstJspJbide1641Test;
 import org.jboss.tools.jst.jsp.test.ca.JstJspJbide1759Test;
 import org.jboss.tools.jst.jsp.test.ca.StrutsJspJbide1648Test;
-
-import junit.framework.Test;
-import junit.framework.TestSuite;
 
 public class JstJspAllTests {
 
@@ -25,6 +26,7 @@ public class JstJspAllTests {
 		suite.addTestSuite(JsfJspJbide1704Test.class);
 		suite.addTestSuite(JsfJspJbide1717Test.class);
 		suite.addTestSuite(JstJspJbide1759Test.class);
+		suite.addTestSuite(Jbide1791Test.class);
 		return suite;
 	}
 
