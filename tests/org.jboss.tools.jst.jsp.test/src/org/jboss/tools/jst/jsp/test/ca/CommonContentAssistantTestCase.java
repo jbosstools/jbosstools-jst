f34a7bc31799885ae360934db1060c1d10edb133
Eugene Stherbin <estherbin@exadel.com>
2008-06-16 11:09:27 +0000

    Fix http://jira.jboss.com/jira/browse/JBIDE-2277 issue
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@8782 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.jst.jsp.test/src/org/jboss/tools/jst/jsp/test/ca/CommonContentAssistantTestCase.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2008 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributor:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+
+
+package org.jboss.tools.jst.jsp.test.ca;
+
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.jboss.tools.common.test.util.TestProjectProvider;
+
+
+/**
+ * Abstract class contains of common methods.
+ * 
+ * @author Eugene Stherbin
+ * 
+ */
+public abstract class CommonContentAssistantTestCase extends ContentAssistantTestCase {
+    /** The make copy. */
+    protected boolean makeCopy;
+
+    /** The provider. */
+    protected TestProjectProvider provider;
+
+    protected abstract String getSetUpProjectName();
+
+    @Override
+    public void setUp() throws Exception {
+        provider = new TestProjectProvider("org.jboss.tools.seam.ui.test", null, getSetUpProjectName(), makeCopy);
+        project = provider.getProject();
+        Throwable exception = null;
+        try {
+            project.refreshLocal(IResource.DEPTH_INFINITE, null);
+        } catch (Exception x) {
+            exception = x;
+            x.printStackTrace();
+        }
+        assertNull("An exception caught: " + (exception != null ? exception.getMessage() : ""), exception);
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        if (provider != null) {
+            provider.dispose();
+        }
+    }
+}
