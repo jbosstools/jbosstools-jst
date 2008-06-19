564a4cc31c0f0f9801003c5fc8323622a109e272
Victor Rubezhny <ruby@exadel.com>
2008-06-19 13:29:07 +0000

    http://jira.jboss.com/jira/browse/JBIDE-2317 Problems with code complete in JSF editor http://jira.jboss.com/jira/browse/JBIDE-2334 JSF EL Code assist should add EL to attribute value but not replace whole not-EL value.
    Issues are fixed
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@8850 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.jst.jsp/src/org/jboss/tools/jst/jsp/contentassist/JSPActiveContentAssistProcessor.java
+++ plugins/org.jboss.tools.jst.jsp/src/org/jboss/tools/jst/jsp/contentassist/JSPActiveContentAssistProcessor.java
@@ -13,6 +13,10 @@ package org.jboss.tools.jst.jsp.contentassist;
 import java.util.Collection;
 import java.util.Iterator;
 
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.wst.sse.core.utils.StringUtils;
 import org.eclipse.wst.xml.core.internal.provisional.document.IDOMNode;
 import org.eclipse.wst.xml.ui.internal.contentassist.ContentAssistRequest;
 import org.eclipse.wst.xml.ui.internal.contentassist.XMLRelevanceConstants;
@@ -73,7 +77,42 @@ public class JSPActiveContentAssistProcessor extends JSPBaseContentAssistProcess
 			tagName = FaceletsHtmlContentAssistProcessor.faceletHtmlPrefixStart + tagName;
 		}
 
-	    String query = new StringBuffer(KbQuery.TAG_SEPARATOR).append(tagName).append(KbQuery.ATTRIBUTE_SEPARATOR).append(attributeName).append(KbQuery.ENUMERATION_SEPARATOR).append(matchString).toString();
+    	if(!currentValue.startsWith("\"") && !currentValue.startsWith("'")) {
+    		// Do not show any value proposals if the attribute value is not started with a quote/double-quote character 
+    		return;
+    	}
+
+		String matchStringForQuery = matchString;
+		String strippedValueForQuery = strippedValue;
+		int elStartPosition = getELStartPosition(matchString);
+		int delta = 0;
+		String proposalPrefix = "";
+		String proposalSufix = "";
+		if (elStartPosition == -1) {
+			if(isCharSharp(matchString, offset-1)) {
+				proposalPrefix = "{";  //$NON-NLS-1$
+			} else {
+				proposalPrefix = "#{";  //$NON-NLS-1$
+			}
+		}
+
+		if (elStartPosition == -1) {
+			matchStringForQuery = "#{";
+			strippedValueForQuery = "";
+			delta = matchString.length();
+		} else {
+			matchStringForQuery = matchString.substring(elStartPosition);
+			strippedValueForQuery = matchStringForQuery;
+	    	delta = matchString.length() - matchStringForQuery.length();
+		}
+		
+    	// Correct delta if matchString starts with a quote (exclude that quote)
+		if (matchString.startsWith("\"") || matchString.startsWith("'")) {
+			strippedValueForQuery = matchString.substring(0,1) + strippedValueForQuery;
+			delta--;
+		}
+    	
+	    String query = new StringBuffer(KbQuery.TAG_SEPARATOR).append(tagName).append(KbQuery.ATTRIBUTE_SEPARATOR).append(attributeName).append(KbQuery.ENUMERATION_SEPARATOR).append(matchStringForQuery).toString();
 	    try {
 			Collection proposals = wtpKbConnector.getProposals(query);
 			if(proposals.size()==0 && htmlQuery!=null) {
@@ -81,7 +120,7 @@ public class JSPActiveContentAssistProcessor extends JSPBaseContentAssistProcess
 			}
             for (Iterator iter = proposals.iterator(); iter.hasNext();) {
             	KbProposal kbProposal = cleanFaceletProposal((KbProposal)iter.next());
-            	kbProposal.postProcess(strippedValue, offset);
+            	kbProposal.postProcess(strippedValueForQuery, offset - delta);
                 int relevance = kbProposal.getRelevance();
                 if(relevance==KbProposal.R_NONE) {
                     relevance = XMLRelevanceConstants.R_XML_ATTRIBUTE_VALUE;
@@ -89,59 +128,39 @@ public class JSPActiveContentAssistProcessor extends JSPBaseContentAssistProcess
                 
                 if(kbProposal.getStart() >= 0) {
         			String replacementString = kbProposal.getReplacementString();
-                    int replacementBeginPosition = contentAssistRequest.getReplacementBeginPosition() + kbProposal.getStart();
+                    int replacementBeginPosition = contentAssistRequest.getReplacementBeginPosition() + kbProposal.getStart() + delta;
                     int replacementLength = kbProposal.getEnd() - kbProposal.getStart();
                 	int cursorPositionDelta = 0;
-                	if(currentValue.startsWith("\"") || currentValue.startsWith("'")) {
-                		replacementBeginPosition = replacementBeginPosition +1;
-            		} else {
-            			cursorPositionDelta++;
-            			replacementString = "\"" + replacementString;
-            		}
-                	if(currentValue.endsWith("\"") || currentValue.endsWith("'")) {
-                		if(currentValue.endsWith(".}\"") && replacementString.endsWith("}")) {
-                			replacementLength--;
-                			replacementString = replacementString.substring(0, replacementString.length()-1);
+                	
+                	// Add an EL-starting quotation characters if needed
+                	if (elStartPosition == -1) {
+                		replacementString = proposalPrefix + replacementString;
+            			cursorPositionDelta += proposalPrefix.length();
+                	}
+  
+                	if((currentValue.length() > StringUtils.strip(currentValue).length()) && 
+                			(currentValue.endsWith("\"") || currentValue.endsWith("'")) ) {
+                		if(!currentValue.endsWith("}\"")) {
+                			replacementString += "}";
                 		}
             		} else {
-            			replacementString += ("\"");
+                		if(elStartPosition == -1 && !currentValue.endsWith("}")) {
+                			replacementString += "}";
+                		}
+//            			replacementString += ("\"");
             		}
                 	int cursorPosition = kbProposal.getPosition() + cursorPositionDelta;
-                	AutoContentAssistantProposal proposal = new AutoContentAssistantProposal(kbProposal.autoActivationContentAssistantAfterApplication(), replacementString,
-                			replacementBeginPosition, replacementLength, cursorPosition, SharedXMLEditorPluginImageHelper.getImage(SharedXMLEditorPluginImageHelper.IMG_OBJ_ATTRIBUTE),
-            				kbProposal.getLabel(), null, kbProposal.getContextInfo(), relevance);
+                	String displayString = elStartPosition == -1 ? 
+                			proposalPrefix + kbProposal.getReplacementString() + "}" :
+                				kbProposal.getReplacementString();
+                	AutoContentAssistantProposal proposal = new AutoContentAssistantProposal(
+                			kbProposal.autoActivationContentAssistantAfterApplication(), 
+                			replacementString,
+                			replacementBeginPosition, replacementLength, cursorPosition, 
+                			SharedXMLEditorPluginImageHelper.getImage(SharedXMLEditorPluginImageHelper.IMG_OBJ_ATTRIBUTE),
+            				displayString, null, kbProposal.getContextInfo(), relevance);
             		contentAssistRequest.addProposal(proposal);
-
-            		continue;
                 }
-                
-                
-            	StringBuffer replacementStringBuffer = new StringBuffer(kbProposal.getReplacementString());
-                int replacementBeginPosition = contentAssistRequest.getReplacementBeginPosition();
-            	int replacementLength = contentAssistRequest.getReplacementLength();
-            	int cursorPositionDelta = 0;
-            	if(currentValue.startsWith("\"") || currentValue.startsWith("'")) {
-            		replacementBeginPosition = replacementBeginPosition +1;
-            		replacementLength--;
-        		} else {
-        			cursorPositionDelta++;
-        			replacementStringBuffer.insert(0, "\"");
-        		}
-            	if(currentValue.endsWith("\"") || currentValue.endsWith("'")) {
-            		replacementLength--;
-            		if(currentValue.endsWith(".}\"") && replacementStringBuffer.toString().endsWith("}")) {
-            			replacementLength--;
-            			replacementStringBuffer = new StringBuffer(replacementStringBuffer.toString().substring(0, replacementStringBuffer.length()-1));
-            		}
-        		} else {
-        			replacementStringBuffer.append("\"");
-        		}
-            	String replacementString = replacementStringBuffer.toString();
-            	int cursorPosition = kbProposal.getPosition() + cursorPositionDelta;
-            	AutoContentAssistantProposal proposal = new AutoContentAssistantProposal(kbProposal.autoActivationContentAssistantAfterApplication(), replacementString,
-            			replacementBeginPosition, replacementLength, cursorPosition, SharedXMLEditorPluginImageHelper.getImage(SharedXMLEditorPluginImageHelper.IMG_OBJ_ATTRIBUTE),
-        				kbProposal.getLabel(), null, kbProposal.getContextInfo(), relevance);
-        		contentAssistRequest.addProposal(proposal);
             }
         } catch (KbException e) {
         	JspEditorPlugin.getPluginLog().logError(e);
@@ -174,4 +193,49 @@ public class JSPActiveContentAssistProcessor extends JSPBaseContentAssistProcess
     public void setFacelets(boolean isFacelets) {
 		this.isFacelets = isFacelets;
 	}
+    
+	/*
+	 * Checks if the EL start starting characters are present
+	 * @return
+	 */
+	private int getELStartPosition(String matchString) {
+		if (matchString == null || matchString.length() == 0)
+			return -1;
+
+		int offset = matchString.length();
+
+		while (--offset >= 0) {
+			if ('}' == matchString.charAt(offset))
+				return -1;
+
+			if ('"' == matchString.charAt(offset) || '\'' == matchString.charAt(offset)) {
+                int backslashCount = 0;
+                while ((offset - 1 - backslashCount) >= 0 && matchString.charAt(offset - 1 - backslashCount) == '\\') {
+                    backslashCount++;
+                }
+                
+                if (backslashCount % 2 == 0)
+                    return -1;
+            }
+
+			if ('{' == matchString.charAt(offset) &&
+					(offset - 1) >= 0 && 
+					('#' == matchString.charAt(offset - 1) || 
+							'$' == matchString.charAt(offset - 1))) {
+				return (offset - 1);
+			}
+		}
+		return -1;
+	}
+
+	/*  Checks if the preceding character is a Sharp-character
+	 */
+	private boolean isCharSharp(String matchString, int offset) {
+		if (matchString == null || offset > matchString.length() || offset < 0) {
+			return false;
+		}
+
+		return '#' == matchString.charAt(offset);
+	}
+
 }
