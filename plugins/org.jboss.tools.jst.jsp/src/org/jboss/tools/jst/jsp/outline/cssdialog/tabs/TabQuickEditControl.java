4fd167b98ad04e57ab491fed72e3c290f9e21c74
Maxim Areshkau <mareshkau@exadel.com>
2009-03-02 13:00:09 +0000

    https://jira.jboss.org/jira/browse/JBIDE-3869
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.0.x@13909 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.jst.jsp/src/org/jboss/tools/jst/jsp/outline/cssdialog/tabs/TabQuickEditControl.java
+++ plugins/org.jboss.tools.jst.jsp/src/org/jboss/tools/jst/jsp/outline/cssdialog/tabs/TabQuickEditControl.java
@@ -114,8 +114,7 @@ public class TabQuickEditControl extends BaseTabControl {
         if(listKeys.size()==0) {
         	label =  new Label(this, SWT.CENTER);
         	label.setText(JstUIMessages.CSS_NO_EDITED_PROPERTIES);
-        }
-        System.out.println();      
+        }    
         Collections.sort(listKeys);
         updateDataFromStyleAttributes = true;
         for (String key : listKeys) {
