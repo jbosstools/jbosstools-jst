1be610966d38505059c03fafd618f87426346f3a
Alexey Kazakov <akazakov@exadel.com>
2009-05-15 12:55:45 +0000

    https://jira.jboss.org/jira/browse/JBIDE-2808
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@15282 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.jst.web.kb/src/org/jboss/tools/jst/web/kb/WebKbPlugin.java
+++ plugins/org.jboss.tools.jst.web.kb/src/org/jboss/tools/jst/web/kb/WebKbPlugin.java
@@ -1,23 +1,23 @@
 package org.jboss.tools.jst.web.kb;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.jboss.tools.common.log.BaseUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class WebKbPlugin extends BaseUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.jboss.tools.jst.web.kb";
 
 	// The shared instance
-	private static Activator plugin;
-	
+	private static WebKbPlugin plugin;
+
 	/**
 	 * The constructor
 	 */
-	public Activator() {
+	public WebKbPlugin() {
 	}
 
 	/*
@@ -43,8 +43,7 @@ public class Activator extends AbstractUIPlugin {
 	 *
 	 * @return the shared instance
 	 */
-	public static Activator getDefault() {
+	public static WebKbPlugin getDefault() {
 		return plugin;
 	}
-
 }
