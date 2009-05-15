00e1ac06ac2005af17aab25a01d217d27ee8a7a0
Alexey Kazakov <akazakov@exadel.com>
2009-05-15 13:23:02 +0000

    https://jira.jboss.org/jira/browse/JBIDE-2808
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@15284 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ tests/org.jboss.tools.jst.web.kb.test/src/org/jboss/tools/jst/web/kb/test/WebKbTest.java
@@ -0,0 +1,29 @@
+/******************************************************************************* 
+ * Copyright (c) 2009 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.jboss.tools.jst.web.kb.test;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * @author Alexey Kazakov
+ */
+public class WebKbTest extends TestCase {
+
+	public void testKb() {
+		//TODO
+	}
+
+	public static Test suite() {
+		return new TestSuite(WebKbTest.class);
+	}
+}
