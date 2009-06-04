d3c29a5ed3a873a1a247659f37b2660098f80726
Viacheslav Kabanovich <scabanovich@exadel.com>
2009-06-04 20:48:29 +0000

    https://jira.jboss.org/jira/browse/JBIDE-2808
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@15721 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.jst.web.test/src/org/jboss/tools/jst/web/test/KbModelTest.java
+++ tests/org.jboss.tools.jst.web.test/src/org/jboss/tools/jst/web/test/KbModelTest.java
@@ -11,7 +11,14 @@
 package org.jboss.tools.jst.web.test;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.jboss.tools.common.test.util.TestProjectProvider;
+import org.jboss.tools.jst.web.kb.IKbProject;
+import org.jboss.tools.jst.web.kb.KbProjectFactory;
+import org.jboss.tools.jst.web.kb.taglib.ITagLibrary;
+import org.jboss.tools.test.util.JUnitUtils;
 
 import junit.framework.TestCase;
 
@@ -21,14 +28,35 @@ public class KbModelTest extends TestCase {
 	IProject project = null;
 	boolean makeCopy = true;
 
-	public KbModelTest() {}
+	public KbModelTest() {
+		super("Kb Model Test");
+	}
 
 	public void setUp() throws Exception {
+		provider = new TestProjectProvider("org.jboss.tools.jst.web.test",
+				null,"TestKbModel" ,true);
+		project = provider.getProject();
+		project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+		this.project.build(IncrementalProjectBuilder.FULL_BUILD, null);
+	}
 
+	private IKbProject getKbProject() {
+		IKbProject kbProject = null;
+		try {
+			kbProject = (IKbProject)project.getNature(IKbProject.NATURE_ID);
+		} catch (Exception e) {
+			JUnitUtils.fail("Cannot get seam nature.",e);
+		}
+		return kbProject;
 	}
 
 	public void testXMLScanner() {
-		
+		IKbProject kbProject = getKbProject();
+		ITagLibrary[] ls = kbProject.getTagLibraries();
+//		System.out.println("Libraries found=" + ls.length);
+//		for (int i = 0; i < ls.length; i++) {
+//			System.out.println(ls[i] + ":=>" + ls[i].getComponents().length + " " + ls[i].getURI());
+//		}
 	}
 
 	public void testKbProjectObjects() {
