64b653e4f588c50c6e72a529a712e1b5caf2ace1
Viacheslav Kabanovich <scabanovich@exadel.com>
2009-06-05 21:13:41 +0000

    https://jira.jboss.org/jira/browse/JBIDE-2808
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@15758 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.jst.web.test/src/org/jboss/tools/jst/web/test/KbModelTest.java
+++ tests/org.jboss.tools.jst.web.test/src/org/jboss/tools/jst/web/test/KbModelTest.java
@@ -10,13 +10,18 @@
  ******************************************************************************/ 
 package org.jboss.tools.jst.web.test;
 
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.jboss.tools.common.test.util.TestProjectProvider;
 import org.jboss.tools.jst.web.kb.IKbProject;
-import org.jboss.tools.jst.web.kb.KbProjectFactory;
+import org.jboss.tools.jst.web.kb.internal.scanner.LoadedDeclarations;
+import org.jboss.tools.jst.web.kb.internal.scanner.ScannerException;
+import org.jboss.tools.jst.web.kb.internal.scanner.XMLScanner;
 import org.jboss.tools.jst.web.kb.taglib.ITagLibrary;
 import org.jboss.tools.test.util.JUnitUtils;
 
@@ -52,7 +57,44 @@ public class KbModelTest extends TestCase {
 
 	public void testXMLScanner() {
 		IKbProject kbProject = getKbProject();
-		ITagLibrary[] ls = kbProject.getTagLibraries();
+		
+		IFile f = project.getFile("WebContent/WEB-INF/faces-config.xml");
+		assertNotNull(f);
+		XMLScanner scanner = new XMLScanner();
+		List<ITagLibrary> ls = null;		
+		try {
+			LoadedDeclarations ds = scanner.parse(f, kbProject);
+			ls = ds.getLibraries();
+		} catch (ScannerException e) {
+			JUnitUtils.fail("Error in xml scanner",e);
+		}
+		assertEquals(1, ls.size());
+		assertTrue(ls.get(0).getComponents().length > 0);
+
+		ls = null;
+		f = project.getFile("WebContent/facelet-taglib.xml");
+		assertNotNull(f);
+		try {
+			LoadedDeclarations ds = scanner.parse(f, kbProject);
+			ls = ds.getLibraries();
+		} catch (ScannerException e) {
+			JUnitUtils.fail("Error in xml scanner",e);
+		}
+		assertEquals(1, ls.size());
+		assertTrue(ls.get(0).getComponents().length > 0);
+		
+		ls = null;
+		f = project.getFile("WebContent/WEB-INF/taglib2.tld");
+		assertNotNull(f);
+		try {
+			LoadedDeclarations ds = scanner.parse(f, kbProject);
+			ls = ds.getLibraries();
+		} catch (ScannerException e) {
+			JUnitUtils.fail("Error in xml scanner",e);
+		}
+		assertEquals(1, ls.size());
+		assertTrue(ls.get(0).getComponents().length > 0);
+		
 //		System.out.println("Libraries found=" + ls.length);
 //		for (int i = 0; i < ls.length; i++) {
 //			System.out.println(ls[i] + ":=>" + ls[i].getComponents().length + " " + ls[i].getURI());
