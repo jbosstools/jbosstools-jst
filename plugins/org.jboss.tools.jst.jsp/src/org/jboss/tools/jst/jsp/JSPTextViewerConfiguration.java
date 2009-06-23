34b8f8e57c155a6eeb68c2004f1f82290d5748dc
Victor Rubezhny <ruby@exadel.com>
2009-06-23 13:56:25 +0000

    JBIDE-4390:  EL isn't highlighted while making OpenOn action on JSP pages.
    The problem is that class org.eclipse.jst.jsp.ui.StructuredTextViewerConfigurationJSP class is probably modified in WTP 3.1M7. So, it doesn't define the LineStyleProvider for the partitionType == "org.eclipse.jst.jsp.SCRIPT.JSP_EL2". This breaks the text regions styling (underlining for our case) for #{...} -like EL regions.
    
    The workaround is performed by providing a "org.eclipse.jst.jsp.SCRIPT.JSP_EL"-like LineStyleProvider for "org.eclipse.jst.jsp.SCRIPT.JSP_EL2" partitions
    
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@16138 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.jst.jsp/src/org/jboss/tools/jst/jsp/JSPTextViewerConfiguration.java
+++ plugins/org.jboss.tools.jst.jsp/src/org/jboss/tools/jst/jsp/JSPTextViewerConfiguration.java
@@ -22,9 +22,12 @@ import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jst.jsp.core.text.IJSPPartitions;
 import org.eclipse.jst.jsp.ui.StructuredTextViewerConfigurationJSP;
+import org.eclipse.jst.jsp.ui.internal.style.jspel.LineStyleProviderForJSPEL;
 import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.ui.texteditor.AbstractDecoratedTextEditorPreferenceConstants;
+import org.eclipse.wst.css.core.text.ICSSPartitions;
 import org.eclipse.wst.html.core.text.IHTMLPartitions;
+import org.eclipse.wst.sse.ui.internal.provisional.style.LineStyleProvider;
 import org.eclipse.wst.xml.core.text.IXMLPartitions;
 import org.osgi.framework.Bundle;
 
@@ -94,6 +97,49 @@ public class JSPTextViewerConfiguration extends StructuredTextViewerConfiguratio
 		return (IContentAssistProcessor[])processors.toArray(new IContentAssistProcessor[0]);
 	}
 
+	
+	/*
+	 * JBIDE-4390:  
+	 * The line style provider for partition type == "org.eclipse.jst.jsp.SCRIPT.JSP_EL2", 
+	 * which is ommitted (forgotten ?) in superclass
+	 */
+	private LineStyleProvider fLineStyleProviderForJSPEL2;
+
+	/*
+	 * JBIDE-4390:  
+	 * The method is overriden to provide a line style provider for partition type == "org.eclipse.jst.jsp.SCRIPT.JSP_EL2", 
+	 * which is ommitted (forgotten ?) in superclass
+	 */
+	private LineStyleProvider getLineStyleProviderForJSPEL2() {
+		if (fLineStyleProviderForJSPEL2 == null) {
+			fLineStyleProviderForJSPEL2 = new LineStyleProviderForJSPEL();
+		}
+		return fLineStyleProviderForJSPEL2;
+	}
+
+	/*
+	 * JBIDE-4390:  
+	 * The method is overriden to provide a line style provider for partition type == "org.eclipse.jst.jsp.SCRIPT.JSP_EL2", 
+	 * which is ommitted (forgotten ?) in superclass
+	 * 
+	 * 
+	 * (non-Javadoc)
+	 * @see org.eclipse.jst.jsp.ui.StructuredTextViewerConfigurationJSP#getLineStyleProviders(org.eclipse.jface.text.source.ISourceViewer, java.lang.String)
+	 */
+	public LineStyleProvider[] getLineStyleProviders(ISourceViewer sourceViewer, String partitionType) {
+		LineStyleProvider[] providers = null;
+
+		if (partitionType == IJSPPartitions.JSP_DEFAULT_EL2) {
+			providers = new LineStyleProvider[]{getLineStyleProviderForJSPEL2()};
+		} 
+		else {
+			providers = super.getLineStyleProviders(sourceViewer, partitionType);
+		}
+
+		return providers;
+	}
+
+	
 	/*
 	 * @see org.eclipse.jface.text.source.SourceViewerConfiguration#getHyperlinkDetectors(org.eclipse.jface.text.source.ISourceViewer)
 	 * @since 3.1
