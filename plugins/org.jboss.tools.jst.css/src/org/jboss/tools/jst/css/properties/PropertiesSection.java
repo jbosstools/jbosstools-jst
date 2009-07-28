5fdff12bd5818c6d7893dec425de455b5a387628
Sergey Dzmitrovich <sdzmitrovich@gmail.com>
2009-07-28 09:17:02 +0000

    revert latest changes
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@16837 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.jst.css/src/org/jboss/tools/jst/css/properties/PropertiesSection.java
+++ plugins/org.jboss.tools.jst.css/src/org/jboss/tools/jst/css/properties/PropertiesSection.java
@@ -12,7 +12,6 @@
 package org.jboss.tools.jst.css.properties;
 
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.internal.views.properties.tabbed.view.TabbedPropertyComposite;
 import org.jboss.tools.jst.jsp.outline.cssdialog.tabs.BaseTabControl;
 import org.jboss.tools.jst.jsp.outline.cssdialog.tabs.TabPropertySheetControl;
 
@@ -25,10 +24,7 @@ public class PropertiesSection extends AbstractCSSSection {
 	@Override
 	public BaseTabControl createSectionControl(Composite parent) {
 
-		//quick fix to solve appearing 2 scrollbars 
-		((TabbedPropertyComposite)getTabComposite()).getScrolledComposite().setExpandVertical(false);
 		return new TabPropertySheetControl(parent, getStyleAttributes(),
 				getBindingContext());
-		
 	}
 }
