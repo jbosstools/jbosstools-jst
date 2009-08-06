7213ff1ca394c6e1b94b147b641b34a21b487615
Viacheslav Kabanovich <scabanovich@exadel.com>
2009-08-06 14:36:42 +0000

    https://jira.jboss.org/jira/browse/JBIDE-3571
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@16984 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.jst.web.kb/src/org/jboss/tools/jst/web/kb/internal/taglib/IDProposalType.java
+++ plugins/org.jboss.tools.jst.web.kb/src/org/jboss/tools/jst/web/kb/internal/taglib/IDProposalType.java
@@ -3,16 +3,11 @@ package org.jboss.tools.jst.web.kb.internal.taglib;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import java.util.StringTokenizer;
 import java.util.TreeSet;
 
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.wst.sse.core.StructuredModelManager;
 import org.eclipse.wst.sse.core.internal.provisional.IStructuredModel;
-import org.eclipse.wst.sse.core.internal.provisional.text.IStructuredDocument;
 import org.eclipse.wst.xml.core.internal.provisional.document.IDOMModel;
 import org.jboss.tools.common.text.TextProposal;
 import org.jboss.tools.jst.web.kb.IPageContext;
@@ -23,6 +18,9 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class IDProposalType extends CustomProposalType {
+	static String ID = "id"; //$NON-NLS-1$
+	static String QUOTE_1 = "'"; //$NON-NLS-1$
+	static String QUOTE_2 = "\""; //$NON-NLS-1$
 	Set<String> idList = new TreeSet<String>();
 
 	@Override
@@ -49,7 +47,7 @@ public class IDProposalType extends CustomProposalType {
 	}
 
 	private void collectIDs(Element element) {
-		String id = element.getAttribute("id");
+		String id = element.getAttribute(ID);
 		if(id != null && id.length() > 0) idList.add(id);
 		NodeList cs = element.getChildNodes();
 		for (int i = 0; i < cs.getLength(); i++) {
@@ -64,8 +62,8 @@ public class IDProposalType extends CustomProposalType {
 	public TextProposal[] getProposals(KbQuery query) {
 		String v = query.getValue();
 		String txt = query.getText();
-		if(txt.startsWith("\"")) txt = txt.substring(1);
-		if(txt.endsWith("\"")) txt = txt.substring(0, txt.length() - 1);
+		if(txt.startsWith(QUOTE_1) || txt.startsWith(QUOTE_2)) txt = txt.substring(1);
+		if(txt.endsWith(QUOTE_1) || txt.endsWith(QUOTE_2)) txt = txt.substring(0, txt.length() - 1);
 		int offset = v.length();
 		int b = v.lastIndexOf(',');
 		if(b < 0) b = 0; else b += 1;
