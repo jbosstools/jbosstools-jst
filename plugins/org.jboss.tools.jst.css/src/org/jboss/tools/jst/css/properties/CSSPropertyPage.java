81dc716d144b01e0002719b38f1081eda695b50a
Sergey Dzmitrovich <sdzmitrovich@gmail.com>
2009-08-28 17:05:23 +0000

    reverting of changes JBIDE-4790
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@17356 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.jst.css/src/org/jboss/tools/jst/css/properties/CSSPropertyPage.java
+++ plugins/org.jboss.tools.jst.css/src/org/jboss/tools/jst/css/properties/CSSPropertyPage.java
@@ -132,12 +132,6 @@ public class CSSPropertyPage extends TabbedPropertySheetPage implements
 							|| (me.getValue().length() == 0)) {
 						declaration.removeProperty(me.getKey());
 					} else {
-
-						// FIX FOR BIDE-4790  simple setting of new
-						// value leads to error in issue's case
-						if (declaration.getPropertyValue(me.getKey()) != null)
-							declaration.removeProperty(me.getKey());
-
 						declaration.setProperty(me.getKey(), me.getValue(),
 								Constants.EMPTY);
 					}
