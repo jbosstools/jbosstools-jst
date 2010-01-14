f5e1c9b4b6cb852f12e3cb61eaf51444b2aa83e8
Alexey Kazakov <akazakov@exadel.com>
2010-01-14 16:31:11 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5650
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@19754 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.jst.web.kb/src/org/jboss/tools/jst/web/kb/taglib/TagLibraryManager.java
+++ plugins/org.jboss.tools.jst.web.kb/src/org/jboss/tools/jst/web/kb/taglib/TagLibraryManager.java
@@ -10,9 +10,15 @@
  ******************************************************************************/ 
 package org.jboss.tools.jst.web.kb.taglib;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 import org.eclipse.core.resources.IProject;
+import org.eclipse.wst.xml.core.internal.XMLCorePlugin;
 import org.jboss.tools.jst.web.kb.IKbProject;
 import org.jboss.tools.jst.web.kb.KbProjectFactory;
+import org.jboss.tools.jst.web.kb.WebKbPlugin;
 
 /**
  * @author Alexey Kazakov
@@ -40,5 +46,31 @@ public class TagLibraryManager {
 	 */
 	public static ITagLibrary[] getLibraries(IProject project) {
 		return getLibraries(project, null);
+	}
+
+	/**
+	 * Returns TLD which are available for all the projects
+	 * even if the lib are not in the project classpath.
+	 * This TLD should be registered in XML Catalog via plugin.xml.
+	 * @param uri
+	 * @return
+	 */
+	public static File getStaticTLD(String uri) {
+		try {
+			if (uri != null) {
+	        	String id = XMLCorePlugin.getDefault().getDefaultXMLCatalog().resolveURI(uri);
+	        	if(id!=null) {
+	        		File file = new File(new URL(id).getFile());
+	        		if(file.exists()) {
+	        			return file;
+	        		}
+	        	}
+			}
+		} catch (IOException e) {
+			WebKbPlugin.getDefault().logError(e);
+			return null;
+		}
+
+		return null;
 	}
 }
