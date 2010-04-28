9c7e3fa11268e239cc4a1105e71b78ba71655e35
Viacheslav Kabanovich <scabanovich@exadel.com>
2010-04-28 13:56:59 +0000

    https://jira.jboss.org/jira/browse/JBIDE-5963
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.1.x@21780 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.jst.jsp/src/org/jboss/tools/jst/jsp/jspeditor/dnd/JSPPaletteInsertHelper.java
+++ plugins/org.jboss.tools.jst.jsp/src/org/jboss/tools/jst/jsp/jspeditor/dnd/JSPPaletteInsertHelper.java
@@ -31,6 +31,7 @@ public class JSPPaletteInsertHelper extends PaletteInsertHelper {
 		String startText = texts[0];
 		if(startText != null && startText.startsWith("<%@ taglib")) { //$NON-NLS-1$
 			if(PaletteTaglibInserter.inserTaglibInXml(v, p)) {
+				texts[0] = "";
 				return;
 			}
 		} else {
