a74bf970cf925b7ec7bd64b4f7ebf4a4c0e6efd6
Denis Golovin <dgolovin@exadel.com>
2010-06-24 23:35:49 +0000

    fix JUnit error in org.jboss.tools.jst.text.ext.test.JsfExtAllTests
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@23020 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.jst.text.ext.test/src/org/jboss/tools/jst/text/ext/test/JsfExtAllTests.java
+++ tests/org.jboss.tools.jst.text.ext.test/src/org/jboss/tools/jst/text/ext/test/JsfExtAllTests.java
@@ -1,27 +1,22 @@
 package org.jboss.tools.jst.text.ext.test;
 
 import junit.framework.Test;
+import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import org.jboss.tools.test.util.ProjectImportTestSetup;
-
-
-public class JsfExtAllTests {
+public class JsfExtAllTests extends TestCase{
 	
 	// all tests were moved to jst.ui.test plugin
 	public void testJsfTextExt () {
 		
 	}
-//	public static Test suite() {
-//		TestSuite suite = new TestSuite("Test for default package");
-//		//$JUnit-BEGIN$
-//
-//		suite.addTest(new ProjectImportTestSetup(new TestSuite(OpenOnsTest.class),
-//				"org.jboss.tools.jst.text.ext.test",
-//				new String[]{"projects/HiperlinksTestProject"},
-//				new String[]{"HiperlinksTestProject"}));
-//		
-//		//$JUnit-END$
-//		return suite;
-//	}
+	public static Test suite() {
+	TestSuite suite = new TestSuite("Test for default package");
+		//$JUnit-BEGIN$
+
+		suite.addTestSuite(JsfExtAllTests.class);
+		
+		//$JUnit-END$
+		return suite;
+	}
 }
