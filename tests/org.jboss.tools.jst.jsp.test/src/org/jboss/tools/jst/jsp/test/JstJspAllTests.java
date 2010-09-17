593eac867e9aa53334d0017c35037eb8a8d93433
Victor Rubezhny <ruby@exadel.com>
2010-09-17 17:51:11 +0000

    JBIDE-6529 : jsf test failures
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@25002 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.jst.jsp.test/src/org/jboss/tools/jst/jsp/test/JstJspAllTests.java
+++ tests/org.jboss.tools.jst.jsp.test/src/org/jboss/tools/jst/jsp/test/JstJspAllTests.java
@@ -4,8 +4,6 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.jboss.tools.jst.jsp.test.ca.Jbide1791Test;
-import org.jboss.tools.jst.jsp.test.ca.JsfJspJbide1704Test;
-import org.jboss.tools.jst.jsp.test.ca.JsfJspJbide1717Test;
 import org.jboss.tools.jst.jsp.test.ca.JstJspJbide1585Test;
 import org.jboss.tools.jst.jsp.test.ca.JstJspJbide1641Test;
 
@@ -15,8 +13,8 @@ public class JstJspAllTests {
 		TestSuite suite = new TestSuite("Test for org.jboss.tools.jst.jsp.test");
  		suite.addTestSuite(JstJspJbide1585Test.class);
 		suite.addTestSuite(JstJspJbide1641Test.class);
-		suite.addTestSuite(JsfJspJbide1704Test.class);
-		suite.addTestSuite(JsfJspJbide1717Test.class);
+//		suite.addTestSuite(JsfJspJbide1704Test.class);
+//		suite.addTestSuite(JsfJspJbide1717Test.class);
 		suite.addTestSuite(Jbide1791Test.class);
 		suite.addTestSuite(JspPreferencesPageTest.class);
 	
