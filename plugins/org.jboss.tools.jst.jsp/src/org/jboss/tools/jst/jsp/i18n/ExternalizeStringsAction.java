3df48c5ebe0359df3a7e983f7b505aecbd85585c
Denis Maliarevich <dmaliarevich@exadel.com>
2010-10-08 10:35:05 +0000

    https://jira.jboss.org/browse/JBIDE-7222 , Adding ExternalizeStrings dialog to the VPE context menu (2).
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@25642 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ plugins/org.jboss.tools.jst.jsp/src/org/jboss/tools/jst/jsp/i18n/ExternalizeStringsAction.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2010 Exadel, Inc. and Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Exadel, Inc. and Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.jst.jsp.i18n;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.TextSelection;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.PlatformUI;
+import org.jboss.tools.jst.jsp.jspeditor.JSPMultiPageEditor;
+import org.jboss.tools.jst.jsp.messages.JstUIMessages;
+
+public class ExternalizeStringsAction extends Action {
+
+	private JSPMultiPageEditor editor;
+	
+	public ExternalizeStringsAction() {
+		super();
+		editor = (JSPMultiPageEditor) PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+	}
+
+	@Override
+	public void run() {
+		ISelection sel = editor.getSelectionProvider().getSelection();
+		if (ExternalizeStringsUtils.isSelectionCorrect(sel)) {
+			/*
+			 * Pass null for Bundle Map that it will be created by the page itself.
+			 */
+			ExternalizeStringsDialog dlg = new ExternalizeStringsDialog(
+					PlatformUI.getWorkbench().getDisplay().getActiveShell(),
+					new ExternalizeStringsWizard(editor.getSourceEditor(), null));
+			dlg.open();
+		} else {
+			MessageDialog.openWarning(
+					PlatformUI.getWorkbench().getDisplay().getActiveShell(),
+					JstUIMessages.EXTERNALIZE_STRINGS_DIALOG_TITLE,
+					JstUIMessages.EXTERNALIZE_STRINGS_DIALOG_WRONG_SELECTION);
+		}
+	}
+
+}
