6f9d31ffae264cec37c808948be1ae2de9558ac4
Victor Rubezhny <ruby@exadel.com>
2010-10-22 12:16:05 +0000

    JBIDE-6397 JstJspAllTests fail intermittently on slave vmg18-rhel5-x86_64 for both JBT & JBDS builds
    JstJspJbide1585Test is included to the suite after JBIDE-1704 is fixed
    
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@25992 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.jst.jsp.test/src/org/jboss/tools/jst/jsp/test/JstJspAllTests.java
+++ tests/org.jboss.tools.jst.jsp.test/src/org/jboss/tools/jst/jsp/test/JstJspAllTests.java
@@ -14,6 +14,7 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 import org.jboss.tools.jst.jsp.test.ca.ExternalizeCommandTest;
+import org.jboss.tools.jst.jsp.test.ca.JstJspJbide1585Test;
 import org.jboss.tools.jst.jsp.test.ca.JstJspJbide1641Test;
 import org.jboss.tools.jst.jsp.test.ca.NewEditorSideBySideCommandTest;
 import org.jboss.tools.jst.jsp.test.ca.SelectionBarTest;
@@ -23,22 +24,14 @@ public class JstJspAllTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Test for org.jboss.tools.jst.jsp.test"); //$NON-NLS-1$
 		
-		/*
-		 * 	TODO: Uncomment the following test case after https://jira.jboss.org/browse/JBIDE-7104 issue 
-		 * is resolved due to enable the test to run
- 		
  		suite.addTestSuite(JstJspJbide1585Test.class);
- 		
- 		*/
- 		
 		suite.addTestSuite(JstJspJbide1641Test.class);
 		
 		/* 
 		 * TODO: Uncomment the following test case after https://jira.jboss.org/browse/JBIDE-7100 issue 
 		 * is resolved due to enable the test to run
-		
+		*
 		suite.addTestSuite(Jbide1791Test.class);
-		
 		*/
 		suite.addTestSuite(JspPreferencesPageTest.class);
 		
