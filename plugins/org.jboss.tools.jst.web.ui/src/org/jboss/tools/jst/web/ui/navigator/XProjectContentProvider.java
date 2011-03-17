80439d8d52642376189b371fb332f5638ae61eee
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-03-17 00:41:57 +0000

    JBIDE-8306 https://issues.jboss.org/browse/JBIDE-8306
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@29836 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.jst.web.ui/src/org/jboss/tools/jst/web/ui/navigator/XProjectContentProvider.java
+++ plugins/org.jboss.tools.jst.web.ui/src/org/jboss/tools/jst/web/ui/navigator/XProjectContentProvider.java
@@ -10,6 +10,9 @@
  ******************************************************************************/ 
 package org.jboss.tools.jst.web.ui.navigator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.jdt.core.IJavaProject;
@@ -22,14 +25,26 @@ import org.jboss.tools.jst.web.ui.navigator.XLabelProvider.RootWrapper;
 public class XProjectContentProvider extends XContentProvider {
 
 	public Object[] getChildren(Object parentElement) {
+		boolean root = false;
 		if(parentElement instanceof RootWrapper) {
+			root = true;
 			parentElement = ((RootWrapper)parentElement).element;
 		}
 		if(parentElement instanceof XModelObject) {
 			XModelObject o = (XModelObject)parentElement;
 			XFilteredTree filteredTree = getFilteredTree(o);
 			if(filteredTree != null) {
-				return filteredTree.getChildren(o);
+				XModelObject[] result = filteredTree.getChildren(o);
+				if(root) {
+					List<XModelObject> list = new ArrayList<XModelObject>();
+					for (XModelObject c: result) {
+						if(c.getFileType() != XModelObject.SYSTEM) {
+							list.add(c);
+						}
+					}
+					return list.toArray(new XModelObject[0]);
+				}
+				return result;
 			}
 			return o.getChildrenForSave();
 		} else if(parentElement instanceof IProject || parentElement instanceof IJavaProject) {
