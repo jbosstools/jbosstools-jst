9e9c7960b596bc398aac9cc0603ce11e5daf53dc
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-04-18 19:27:56 +0000

    JBIDE-8744 https://issues.jboss.org/browse/JBIDE-8744
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@30645 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.jst.web.kb/src/org/jboss/tools/jst/web/kb/internal/taglib/FunctionTagLib.java
+++ plugins/org.jboss.tools.jst.web.kb/src/org/jboss/tools/jst/web/kb/internal/taglib/FunctionTagLib.java
@@ -1,3 +1,13 @@
+/******************************************************************************* 
+ * Copyright (c) 2009 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
 package org.jboss.tools.jst.web.kb.internal.taglib;
 
 import java.util.ArrayList;
@@ -14,6 +24,11 @@ import org.jboss.tools.jst.web.kb.taglib.IELFunction;
 import org.jboss.tools.jst.web.kb.taglib.IFunctionLibrary;
 import org.w3c.dom.Element;
 
+/**
+ * 
+ * @author Viacheslav Kabanovich
+ *
+ */
 public abstract class FunctionTagLib extends AbstractTagLib implements IFunctionLibrary {
 	protected List<ELFunction> functions = new ArrayList<ELFunction>();
 	protected IELFunction[] functionArray = null;
