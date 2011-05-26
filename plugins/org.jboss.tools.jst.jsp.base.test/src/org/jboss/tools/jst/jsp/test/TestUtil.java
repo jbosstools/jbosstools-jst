5afb5201053afeb4467937614d0d5f0adf913ae1
Alexey Kazakov <akazakov@exadel.com>
2011-05-26 19:43:02 +0000

    https://issues.jboss.org/browse/JBIDE-8576
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@31527 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.jst.jsp.base.test/src/org/jboss/tools/jst/jsp/test/TestUtil.java
+++ plugins/org.jboss.tools.jst.jsp.base.test/src/org/jboss/tools/jst/jsp/test/TestUtil.java
@@ -1,7 +1,20 @@
 package org.jboss.tools.jst.jsp.test;
 
-import org.jboss.tools.common.base.test.contentassist.CATestUtil;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.CoreException;
+import org.jboss.tools.jst.web.kb.internal.validation.ValidatorManager;
+import org.jboss.tools.test.util.JobUtils;
 
-public class TestUtil extends CATestUtil {
+public class TestUtil {
 
+	public static void waitForValidation(IProject project) throws CoreException{
+		JobUtils.waitForIdle();
+		for (int i = 0; i < 50; i++) {
+			if(ValidatorManager.getStatus().equals(ValidatorManager.SLEEPING)) {
+				break;
+			}
+			JobUtils.delay(100);
+			JobUtils.waitForIdle();
+		}
+	}
 }
