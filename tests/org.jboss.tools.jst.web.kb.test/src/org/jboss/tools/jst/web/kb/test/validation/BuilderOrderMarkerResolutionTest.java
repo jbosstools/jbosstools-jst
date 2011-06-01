eec7b870edecc32831b0e4e5affe3debfdd6a7d6
Viacheslav Kabanovich <scabanovich@exadel.com>
2011-06-01 21:45:38 +0000

    JBIDE-9028 https://issues.jboss.org/browse/JBIDE-9028
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@31758 a97e2381-89e5-4abb-bab3-167db6db766c

--- tests/org.jboss.tools.jst.web.kb.test/src/org/jboss/tools/jst/web/kb/test/validation/BuilderOrderMarkerResolutionTest.java
+++ tests/org.jboss.tools.jst.web.kb.test/src/org/jboss/tools/jst/web/kb/test/validation/BuilderOrderMarkerResolutionTest.java
@@ -12,15 +12,19 @@ package org.jboss.tools.jst.web.kb.test.validation;
 
 import junit.framework.TestCase;
 
+import org.eclipse.core.internal.preferences.EclipsePreferences;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IncrementalProjectBuilder;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.ui.IMarkerResolution;
 import org.eclipse.ui.ide.IDE;
-import org.eclipse.wst.validation.internal.plugin.ValidationPlugin;
+import org.jboss.tools.common.preferences.SeverityPreferences;
 import org.jboss.tools.jst.web.kb.internal.validation.ValidatorManager;
+import org.jboss.tools.jst.web.kb.preferences.ELSeverityPreferences;
 import org.jboss.tools.test.util.JobUtils;
 
 public class BuilderOrderMarkerResolutionTest extends TestCase {
@@ -34,7 +38,8 @@ public class BuilderOrderMarkerResolutionTest extends TestCase {
 
 	private void checkResolution(IProject project, String markerType, String resolutionClassName) throws CoreException {
 		try{
-			IMarker[] markers = project.findMarkers(ValidationPlugin.VALIDATION_BUILDER_ID, true, IResource.DEPTH_ZERO);
+			IMarker[] markers = getBuilderOrderMarkers();
+			assertTrue(markers.length > 0);
 			for (int i = 0; i < markers.length; i++) {
 				IMarker marker = markers[i];
 				IMarkerResolution[] resolutions = IDE.getMarkerHelpRegistry().getResolutions(marker);
@@ -54,6 +59,38 @@ public class BuilderOrderMarkerResolutionTest extends TestCase {
 			JobUtils.waitForIdle();
 //			TestUtil.waitForValidation(project);
 		}
+	}
+
+	private IMarker[] getBuilderOrderMarkers() throws CoreException {
+		return project.findMarkers(ValidatorManager.ORDER_PROBLEM_MARKER_TYPE, true, IResource.DEPTH_ZERO);
+	}
+
+	public void testWrongBuildOrderPreference() throws CoreException {
+		IMarker[] markers = getBuilderOrderMarkers();
+		assertEquals(1, markers.length);
+		assertEquals(IMarker.SEVERITY_ERROR, markers[0].getAttribute(IMarker.SEVERITY, -1));
+
+		modifyPreference(SeverityPreferences.IGNORE);
+		markers = getBuilderOrderMarkers();
+		assertEquals(0, markers.length);
+
+		modifyPreference(SeverityPreferences.WARNING);
+		markers = getBuilderOrderMarkers();
+		assertEquals(1, markers.length);
+		assertEquals(IMarker.SEVERITY_WARNING, markers[0].getAttribute(IMarker.SEVERITY, -1));
+
+		modifyPreference(SeverityPreferences.ERROR);
+		markers = getBuilderOrderMarkers();
+		assertEquals(1, markers.length);
+		assertEquals(IMarker.SEVERITY_ERROR, markers[0].getAttribute(IMarker.SEVERITY, -1));
+	}
+
+	void modifyPreference(String value) throws CoreException {
+		EclipsePreferences ps = (EclipsePreferences)ELSeverityPreferences.getInstance().getProjectPreferences(project);
+		ps.put(ELSeverityPreferences.WRONG_BUILDER_ORDER_PREFERENCE_NAME, value);
+		JobUtils.waitForIdle();
+		project.build(IncrementalProjectBuilder.FULL_BUILD, new NullProgressMonitor());
+		JobUtils.waitForIdle();
 	}
 
 	public void testBuilderOrderResolution() throws CoreException {
