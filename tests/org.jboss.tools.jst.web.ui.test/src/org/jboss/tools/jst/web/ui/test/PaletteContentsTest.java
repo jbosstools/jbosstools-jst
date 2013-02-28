54609526a4c3519c14fca6ad7785a3636b014d37
Viacheslav Kabanovich <scabanovich@exadel.com>
2013-02-28 13:08:55 -0800

    JBIDE-13679 HTML5 Palette should be available for any HTML5 file
    Test is added.

---
+++ tests/org.jboss.tools.jst.web.ui.test/src/org/jboss/tools/jst/web/ui/test/PaletteContentsTest.java
@@ -0,0 +1,54 @@
+package org.jboss.tools.jst.web.ui.test;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.ui.IEditorPart;
+import org.jboss.tools.common.model.ui.views.palette.PaletteContents;
+import org.jboss.tools.test.util.WorkbenchUtils;
+
+import junit.framework.TestCase;
+
+public class PaletteContentsTest extends TestCase {
+	protected IProject project = null;
+	protected IEditorPart editor = null;
+
+	public PaletteContentsTest() {}
+
+	public void setUp() {
+		project = ResourcesPlugin.getWorkspace().getRoot().getProject("SimpleProject");
+	}
+
+	protected void tearDown() throws Exception {
+		if(editor != null) {
+			editor.getSite().getPage().closeEditor(editor, false);
+			editor = null;
+		}
+		super.tearDown();
+	}
+
+	public void testMobilePalette() {
+		openEditor("a.html");
+		PaletteContents p = new PaletteContents(editor);
+		String[] x = p.getNatureTypes();
+		assertEquals(1, x.length);
+		assertEquals("mobile", x[0]);
+	}
+
+	public void testJSFPalette() {
+		openEditor("c.html");
+		PaletteContents p = new PaletteContents(editor);
+		String[] x = p.getNatureTypes();
+		assertEquals(1, x.length);
+		assertEquals("jsf", x[0]);
+	}
+
+	public IEditorPart openEditor(String fileName) {
+		IFile testfile = project.getFile(fileName);
+		assertTrue("Test file doesn't exist: " + project.getName() + "/" + fileName, 
+				(testfile.exists() && testfile.isAccessible()));
+
+		return editor = WorkbenchUtils.openEditor(project.getName()+"/"+ fileName); //$NON-NLS-1$
+	}
+
+}
