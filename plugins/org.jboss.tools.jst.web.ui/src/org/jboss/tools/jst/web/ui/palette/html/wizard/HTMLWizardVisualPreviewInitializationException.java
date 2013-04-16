19308ee55725d625083ab07a7cc9be54af34f065
Alexey Kazakov <akazakov@exadel.com>
2013-04-16 12:04:09 -0700

    JBIDE-14031 jst.web.ui.tests are killed by timeout

---
+++ plugins/org.jboss.tools.jst.web.ui/src/org/jboss/tools/jst/web/ui/palette/html/wizard/HTMLWizardVisualPreviewInitializationException.java
@@ -0,0 +1,57 @@
+/******************************************************************************* 
+ * Copyright (c) 2013 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.jboss.tools.jst.web.ui.palette.html.wizard;
+
+/**
+ * @author Alexey Kazakov
+ */
+public class HTMLWizardVisualPreviewInitializationException extends Exception {
+
+	private static final long serialVersionUID = 1977744483393866114L;
+
+	public HTMLWizardVisualPreviewInitializationException() {
+	}
+
+	/**
+	 * @param message
+	 */
+	public HTMLWizardVisualPreviewInitializationException(String message) {
+		super(message);
+	}
+
+	/**
+	 * @param cause
+	 */
+	public HTMLWizardVisualPreviewInitializationException(Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public HTMLWizardVisualPreviewInitializationException(String message,
+			Throwable cause) {
+		super(message, cause);
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
+	public HTMLWizardVisualPreviewInitializationException(String message,
+			Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+}
