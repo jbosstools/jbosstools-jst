6aa2a21550a310a415e2a808df65586435403961
Alexey Kazakov <akazakov@exadel.com>
2014-01-08 18:35:14 -0800

    JBIDE-16294 Added data-dialog="true" attribute to Dialog

---
+++ plugins/org.jboss.tools.jst.web.kb/src/org/jboss/tools/jst/web/kb/internal/taglib/html/jq/DialogAttributeProvider14.java
@@ -0,0 +1,50 @@
+/******************************************************************************* 
+ * Copyright (c) 2014 Red Hat, Inc. 
+ * Distributed under license by Red Hat, Inc. All rights reserved. 
+ * This program is made available under the terms of the 
+ * Eclipse Public License v1.0 which accompanies this distribution, 
+ * and is available at http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: 
+ * Red Hat, Inc. - initial API and implementation 
+ ******************************************************************************/ 
+package org.jboss.tools.jst.web.kb.internal.taglib.html.jq;
+
+import static org.jboss.tools.jst.web.kb.internal.taglib.html.jq.JQueryMobileAttrConstants14.*;
+
+import org.jboss.tools.jst.web.kb.internal.taglib.CustomTagLibAttribute;
+import org.jboss.tools.jst.web.kb.internal.taglib.html.HtmlAttribute;
+
+/**
+ * @author Alexey Kazakov
+ */
+public class DialogAttributeProvider14 extends DialogAttributeProvider {
+
+	static final HtmlAttribute[] REQUIRED_ATTRIBUTES = new HtmlAttribute[] {DATA_DIALOG_ATTRIBUTE};
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.jboss.tools.jst.web.kb.internal.taglib.attribute.AbstractAttributeProvider#checkComponent(org.jboss.tools.jst.web.kb.KbQuery)
+	 */
+	@Override
+	protected boolean checkComponent() {
+		return checkDataRole(PAGE);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.jboss.tools.jst.web.kb.internal.taglib.attribute.AbstractAttributeProvider#getAttributes()
+	 */
+	@Override
+	protected CustomTagLibAttribute[] getConditionalAttributes() {
+		if(checkAttribute(DATA_DIALOG_ATTRIBUTE.getName(), "true")) {
+			return CONDITIONAL_ATTRIBUTES;
+		}
+		return REQUIRED_ATTRIBUTES;
+	}
+
+	@Override
+	protected CustomTagLibAttribute[] getRequiredAttributes() {
+		return EMPTY;
+	}
+}
