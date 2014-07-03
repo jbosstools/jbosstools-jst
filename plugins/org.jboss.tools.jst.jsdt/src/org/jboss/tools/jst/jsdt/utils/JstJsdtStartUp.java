ac56137e847192f202020e5ecfeaf536df6f7d8c
vrubezhny <vrubezhny@exadel.com>
2014-07-03 23:14:35 +0400

    JBIDE-17674 Remove 'wrong Node.js install' workaround
    Workaround is removed
    
    Signed-off-by: vrubezhny <vrubezhny@exadel.com>

--- plugins/org.jboss.tools.jst.jsdt/src/org/jboss/tools/jst/jsdt/utils/JstJsdtStartUp.java
+++ plugins/org.jboss.tools.jst.jsdt/src/org/jboss/tools/jst/jsdt/utils/JstJsdtStartUp.java
@@ -10,20 +10,7 @@
  *******************************************************************************/
 package org.jboss.tools.jst.jsdt.utils;
 
-import java.io.IOException;
-
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.ui.IStartup;
-import org.eclipse.ui.preferences.ScopedPreferenceStore;
-
-import tern.eclipse.ide.core.preferences.PreferencesSupport;
-import tern.eclipse.ide.server.nodejs.core.INodejsInstall;
-import tern.eclipse.ide.server.nodejs.core.INodejsInstallManager;
-import tern.eclipse.ide.server.nodejs.core.TernNodejsCoreConstants;
-import tern.eclipse.ide.server.nodejs.core.TernNodejsCorePlugin;
 
 /**
  * 
@@ -31,67 +18,9 @@ import tern.eclipse.ide.server.nodejs.core.TernNodejsCorePlugin;
  *
  */
 public class JstJsdtStartUp implements IStartup {
-	private static final String IGNORE_RUNTIME_OVERRIDE_PREFERENCE = 
-			"org.jboss.tools.jst.jsdt.ignoreRuntimeOverride";
-	
+
 	@Override
 	public void earlyStartup() {
-		ScopedPreferenceStore store = new ScopedPreferenceStore(
-				InstanceScope.INSTANCE,
-				"org.jboss.tools.jst.jsdt");
-		
-		// Node.js install setup is already performed
-		if (store.getBoolean(IGNORE_RUNTIME_OVERRIDE_PREFERENCE))
-			return;
-
-		try {
-			// Check Node.js install (there should be used NON-native install
-			// only)
-			PreferencesSupport preferencesSupport = new PreferencesSupport(
-					TernNodejsCorePlugin.PLUGIN_ID, TernNodejsCorePlugin
-							.getDefault().getPluginPreferences());
-			String id = preferencesSupport
-					.getWorkspacePreferencesValue(TernNodejsCoreConstants.NODEJS_INSTALL);
-
-			INodejsInstall currentInstall = id == null ? null
-					: TernNodejsCorePlugin.getNodejsInstallManager()
-							.findNodejsInstall(id);
-
-			if (currentInstall == null || currentInstall.isNative()) {
-				// Set up NON-Native Node.js install (this will force to use
-				// OOTB
-				// Node.js instance)
-				INodejsInstallManager installManager = TernNodejsCorePlugin
-						.getNodejsInstallManager();
-				INodejsInstall[] installs = installManager.getNodejsInstalls();
-				for (INodejsInstall install : installs) {
-					if (!install.isNative()) {
-						ScopedPreferenceStore node = new ScopedPreferenceStore(
-								InstanceScope.INSTANCE,
-								TernNodejsCorePlugin.PLUGIN_ID);
-
-						node.putValue(TernNodejsCoreConstants.NODEJS_INSTALL,
-								install.getId());
-						node.save();
-						break;
-					}
-				}
-			}
-		} catch (IOException ex) {
-			Platform.getLog(Platform.getBundle(TernNodejsCorePlugin.PLUGIN_ID))
-					.log(new Status(IStatus.ERROR,
-							TernNodejsCorePlugin.PLUGIN_ID, ex.getMessage(), ex));
-		} finally {
-			// We have made at least one try to configure Node.js OOTB install,
-			// no more more config changes allowed
-			store.setValue(IGNORE_RUNTIME_OVERRIDE_PREFERENCE, true);
-			try {
-				store.save();
-			} catch (IOException e) {
-				Platform.getLog(Platform.getBundle(TernNodejsCorePlugin.PLUGIN_ID))
-						.log(new Status(IStatus.ERROR,
-								TernNodejsCorePlugin.PLUGIN_ID, e.getMessage(), e));
-			}
-		}
+		// Empty, but exists here to make sure that early start up takes place
 	}
 }
