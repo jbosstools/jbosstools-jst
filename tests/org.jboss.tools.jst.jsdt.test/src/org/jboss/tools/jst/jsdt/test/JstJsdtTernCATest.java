0c1f033b952406e91162ea9117f77fb9e8bd178b
vrubezhny <vrubezhny@exadel.com>
2015-01-23 04:58:12 +0300

    JBIDE-19078 - o.j.t.jst.jsdt.test JUnit Test Case failure
    JUnit test is fixed
    
    Signed-off-by: vrubezhny <vrubezhny@exadel.com>

--- tests/org.jboss.tools.jst.jsdt.test/src/org/jboss/tools/jst/jsdt/test/JstJsdtTernCATest.java
+++ tests/org.jboss.tools.jst.jsdt.test/src/org/jboss/tools/jst/jsdt/test/JstJsdtTernCATest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Red Hat, Inc.
+ * Copyright (c) 2014-2015 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v1.0 which accompanies this distribution,
@@ -10,7 +10,10 @@
  ******************************************************************************/
 package org.jboss.tools.jst.jsdt.test;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
@@ -33,9 +36,14 @@ public class JstJsdtTernCATest extends ContentAssistantTestCase {
 	private static final String PROJECT_NAME = "JavaScriptProject"; //$NON-NLS-1$
 	private static final String PAGE_NAME = "WebContent/index.html"; //$NON-NLS-1$
 	private static final String ECMA5_PREFIX_STRING = "Boolean"; //$NON-NLS-1$
-	private static final String ECMA5_PROPOSAL_STRING = "Boolean(value)";  //$NON-NLS-1$
+	private static final String[] ECMA5_PROPOSAL_STRINGS = {
+		"Boolean : fn - ecma5",  //$NON-NLS-1$
+		"Boolean(value) : bool - ecma5"  //$NON-NLS-1$
+	};
 	private static final String BROWSER_PREFIX_STRING = "CanvasRenderingContext2D"; //$NON-NLS-1$
-	private static final String BROWSER_PROPOSAL_STRING = "CanvasRenderingContext2D"; //$NON-NLS-1$
+	private static final String[] BROWSER_PROPOSAL_STRINGS = {
+		"CanvasRenderingContext2D : CanvasRenderingContext2D - browser" //$NON-NLS-1$
+	};
 	
 	public static Test suite() {
 		return new TestSuite(JstJsdtAllTests.class);
@@ -50,14 +58,14 @@ public class JstJsdtTernCATest extends ContentAssistantTestCase {
 	}
 
 	public void testJstJsdtTernCAOnEcma5() {
-		doJstJsdtTernCATest(PAGE_NAME, ECMA5_PREFIX_STRING, ECMA5_PROPOSAL_STRING);
+		doJstJsdtTernCATest(PAGE_NAME, ECMA5_PREFIX_STRING, ECMA5_PROPOSAL_STRINGS);
 	}
 
 	public void testJstJsdtTernCAOnBrowser() {
-		doJstJsdtTernCATest(PAGE_NAME, BROWSER_PREFIX_STRING, BROWSER_PROPOSAL_STRING);
+		doJstJsdtTernCATest(PAGE_NAME, BROWSER_PREFIX_STRING, BROWSER_PROPOSAL_STRINGS);
 	}
 
-	private void doJstJsdtTernCATest(String pageName, String prefix, String value) {
+	private void doJstJsdtTernCATest(String pageName, String prefix, String[] values) {
 		openEditor(pageName);
 		try {		
 			String documentContent = document.get();
@@ -70,17 +78,38 @@ public class JstJsdtTernCATest extends ContentAssistantTestCase {
 	
 			assertTrue("Content Assistant returned no proposals", (res != null && res.size() > 0)); //$NON-NLS-1$
 			
+			Set<String> vals = new HashSet<String>();
+			vals.addAll(Arrays.asList(values));
+					
 			for (ICompletionProposal p : res) {
 				assertTrue("Content Assistant returned proposals which type (" + p.getClass().getName() + ") differs from JSTernCompletionProposal", (p instanceof JSTernCompletionProposal));  //$NON-NLS-1$//$NON-NLS-2$
 				
 				JSTernCompletionProposal proposal = (JSTernCompletionProposal)p;
-				String proposalString = proposal.getReplacementString();
-			
-				assertTrue("The proposal Replacement String is not expected: [" + proposalString + "]", value.equals(proposalString)); 
+				String proposalString = proposal.getDisplayString();
+				for (String value : values) {
+					if (value.equals(proposalString)) {
+						vals.remove(value);
+					}
+				}
+				if (vals.size() == 0) {
+					break;
+				}
 			}
+			
+			String rest = "";
+			if (vals.size() > 0) {
+				boolean first = true;
+				for (String val : vals) {
+					if (!first) {
+						rest += ", ";
+						first = false;
+					}
+					rest += "[" + val + "]";
+				}
+			}
+			assertEquals("The proposal Display Strings is/are not found: " + rest, 0, vals.size()); 
 		} finally {
 			closeEditor();
 		}
 	}
-
 }
