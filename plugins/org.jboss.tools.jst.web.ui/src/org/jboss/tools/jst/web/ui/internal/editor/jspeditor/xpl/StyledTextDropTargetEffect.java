8d6ec007421a44398912f13a17b0374713c831ce
dgolovin <dgolovin@gmail.com>
2015-09-15 12:16:10 -0700

    JBIDE-20690 DnD over VPE source part fails under Mac OS X and partially frozen UI
    Fix check for negative offcet before asking editor for location

--- plugins/org.jboss.tools.jst.web.ui/src/org/jboss/tools/jst/web/ui/internal/editor/jspeditor/xpl/StyledTextDropTargetEffect.java
+++ plugins/org.jboss.tools.jst.web.ui/src/org/jboss/tools/jst/web/ui/internal/editor/jspeditor/xpl/StyledTextDropTargetEffect.java
@@ -155,6 +155,7 @@ public class StyledTextDropTargetEffect extends DropTargetEffect {
 		
 		Point mousePoint = text.getDisplay().map(null, text, event.x, event.y);
 		Point newOffsetPoint = text.getLocationAtOffset(newOffset);
+		if (newOffset < 0) return;
 		Point pt = mousePoint;
 		Rectangle ca = text.getClientArea();
 		ca.height -= text.getLineHeight();
