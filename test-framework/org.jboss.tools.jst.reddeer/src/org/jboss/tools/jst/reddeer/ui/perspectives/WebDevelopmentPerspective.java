6ff544ac2354f667bcf4f405ea65018abe518f87
Pavol Srna <psrna@redhat.com>
2017-02-16 14:30:09 +0100

    JBIDE-23828 - Move jst.reddeer plugin from integration-tests to jbosstools-jst

---
+++ test-framework/org.jboss.tools.jst.reddeer/src/org/jboss/tools/jst/reddeer/ui/perspectives/WebDevelopmentPerspective.java
@@ -0,0 +1,30 @@
+/******************************************************************************* 
+ * Copyright (c) 2017 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.jst.reddeer.ui.perspectives;
+
+import org.jboss.reddeer.eclipse.ui.perspectives.AbstractPerspective;
+
+/**
+ * Web Development perspective implementation
+ * 
+ * @author vpakan
+ *
+ */
+public class WebDevelopmentPerspective extends AbstractPerspective {
+
+	/**
+	 * Constructs the perspective with "Web Development".
+	 */
+	public WebDevelopmentPerspective() {
+		super("Web Development");
+	}
+
+}
---
+++ test-framework/org.jboss.tools.jst.reddeer/src/org/jboss/tools/jst/reddeer/web/ui/NewXHTMLWizard.java
@@ -0,0 +1,25 @@
+/******************************************************************************* 
+ * Copyright (c) 2017 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.jst.reddeer.web.ui;
+
+import org.jboss.reddeer.jface.wizard.NewWizardDialog;
+/**
+ * New XHTML page wizard
+ * @author vpakan
+ *
+ */
+public class NewXHTMLWizard extends NewWizardDialog{
+	
+	public NewXHTMLWizard(){
+		super("JBoss Tools Web","XHTML Page");
+	}
+
+}
